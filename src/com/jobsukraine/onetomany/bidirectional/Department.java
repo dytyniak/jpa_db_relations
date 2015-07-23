2fcbb996ddffa58aa29a25be8491848a134892af
Dytyniak <vadym.dytyniak@gmail.com>
2015-07-23 11:14:00 +0300

    Add classes in persistence.xml

---
+++ src/com/jobsukraine/onetomany/bidirectional/Department.java
@@ -0,0 +1,10 @@
+package com.jobsukraine.onetomany.bidirectional;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Department {
+	@Id
+	private long id;
+}
---
+++ src/com/jobsukraine/onetomany/bidirectional/Employee.java
@@ -0,0 +1,11 @@
+package com.jobsukraine.onetomany.bidirectional;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	private long id;
+	
+}
